7f525536bca6ada579c8a603ded0ac3bd39890a5
shaxruxtleubaev <tleubaevshaxrux@outlook.com>
2023-08-09 22:19:39 +0500

    some changes

--- src/settings.py
+++ src/settings.py
@@ -122,7 +122,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join('static')
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
@@ -132,7 +132,7 @@ STORAGES = {
     },
 }
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
